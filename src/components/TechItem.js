ba0451bdd3a162d8055ac4b2f0424d41862ef6c0
Rosalvo Júnior <rosalvo@enesolucoes.com.br>
2019-09-30 11:28:51 -0300

    Propriedades do React

---
+++ src/components/TechItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function TechItem({ tech, onDelete }) {
+  return (
+    <li>
+      {tech}
+      <button onClick={onDelete} type="button">
+        Remover
+      </button>
+    </li>
+  );
+}
